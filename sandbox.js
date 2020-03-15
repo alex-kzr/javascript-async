4f31d1ca9207c0c836448c31aa4032f65424d028
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-15 14:15:00 +1000

    Add response status

--- sandbox.js
+++ sandbox.js
@@ -1,12 +1,12 @@
 const request = new XMLHttpRequest();
 
 request.addEventListener('readystatechange', () => {
-    if(request.readyState === 4){
+    if(request.readyState === 4 && request.status === 200){
         console.log(request.responseText);
-    }    
+    }else if(request.readyState === 4){
+        console.log('could not fetch the data');
+    } 
 });
 
 request.open('GET','https://jsonplaceholder.typicode.com/todos/');
 request.send();
-
-console.log(4);
