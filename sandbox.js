31d564254eac39b1c4710e8c813bce1c8dd34434
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-15 17:41:22 +1000

    Alter callbacks to promise

--- sandbox.js
+++ sandbox.js
@@ -1,47 +1,24 @@
-const getTodos = (resource, callback) => {
-    const request = new XMLHttpRequest();
+const getTodos = (resource) => {
+    
+    return new Promise((resolve, reject) =>{
+        const request = new XMLHttpRequest();
 
-    request.addEventListener('readystatechange', () => {
-        if(request.readyState === 4 && request.status === 200){
-            const data = JSON.parse(request.responseText);
-            callback(undefined, data);
-        }else if(request.readyState === 4){
-            // console.log();
-            callback('could not fetch the data', undefined);
-        } 
+        request.addEventListener('readystatechange', () => {
+            if(request.readyState === 4 && request.status === 200){
+                const data = JSON.parse(request.responseText);
+                resolve(data);
+            }else if(request.readyState === 4){
+                reject('error getting resource');
+            } 
     });
 
     request.open('GET', resource);
     request.send();
-};
-
-// getTodos('todos/alex.json', (err, data) => {
-//     console.log(data);
-//     getTodos('todos/luigi.json', (err, data) => {
-//         console.log(data);
-//         getTodos('todos/mario.json', (err, data) => {
-//             console.log(data);    
-//         });    
-//     });   
-// });
-
-// promise example
-const getSomething = () => {
-    return new Promise((resolve, reject) => {
-        // fetch something
-        resolve('some data');
-        //reject('some error');
     });
 };
 
-// getSomething().then(data => {
-//     console.log(data);
-// }, error => {
-//     console.log(error);
-// });
-
-getSomething().then(data => {
-    console.log(data);
+getTodos('todos/alex.json').then(data => {
+    console.log('promise resolved:', data);
 }).catch(err => {
-    console.log(err);
+    console.log('promise rejected:', err);
 });
