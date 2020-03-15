a3b47426d34c251f242eb54880b44b0796b4df2a
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-15 17:19:06 +1000

    Add callback hell

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,4 @@
-const getTodos = callback => {
+const getTodos = (resource, callback) => {
     const request = new XMLHttpRequest();
 
     request.addEventListener('readystatechange', () => {
@@ -11,23 +11,16 @@ const getTodos = callback => {
         } 
     });
 
-    request.open('GET','todos.json');
+    request.open('GET', resource);
     request.send();
 };
 
-console.log(1);
-console.log(2);
-
-getTodos((err, data) => {
-    console.log('callback fired');
-    if(err){
-        console.log(err);
-    }else{
+getTodos('todos/alex.json', (err, data) => {
+    console.log(data);
+    getTodos('todos/luigi.json', (err, data) => {
         console.log(data);
-    }
-    
-    console.log();
+        getTodos('todos/mario.json', (err, data) => {
+            console.log(data);    
+        });    
+    });   
 });
-
-console.log(3);
-console.log(4);
