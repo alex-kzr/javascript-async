75fc9d964f3552a05a8ed800086ba4423d8ac60a
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-15 17:50:19 +1000

    Add chaining promises

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,4 @@
-const getTodos = (resource) => {
+const getTodos = resource => {
     
     return new Promise((resolve, reject) =>{
         const request = new XMLHttpRequest();
@@ -18,7 +18,13 @@ const getTodos = (resource) => {
 };
 
 getTodos('todos/alex.json').then(data => {
-    console.log('promise resolved:', data);
+    console.log('promise 1 resolved:', data);
+    return getTodos('todos/mario.json');
+}).then(data => {
+    console.log('promise 2 resolved:', data);
+    return getTodos('todos/luigi.json');
+}).then(data => {
+    console.log('promise 3 resolved:', data);
 }).catch(err => {
     console.log('promise rejected:', err);
 });
