3c5b260a414e6c34fa145d889eebd3051a1177a1
Alex.Kozyura <aleksey.kozyura@gmail.com>
2020-03-15 21:46:39 +1000

    Add throwing and catching errors

--- sandbox.js
+++ sandbox.js
@@ -1,19 +1,16 @@
 // asynk & await
 
 const getTodos = async () => {
-    const response = await fetch('todos/alex.json');
+    const response = await fetch('todos/axlex.json');
+
+    if(response.status !== 200){
+        throw new Error('cannot fetch the data');
+    }
+
     const data = await response.json();
     return data;
 };
 
 getTodos()
-    .then(data => console.log('resolved:', data));
-
-// fetch('todos/alex.json').then(response => {
-//     console.log('resolved', response);
-//     return response.json();
-// }).then(data => {
-//     console.log(data);
-// }).catch(err => {
-//     console.log('rejected', err);
-// });
+    .then(data => console.log('resolved:', data))
+    .catch(err => console.log('rejected:', err.message));
